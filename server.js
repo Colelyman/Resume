ebe6198ee53621215920da437bf854dce8e9d3c3
Cole Lyman <cole@colelyman.com>
2015-09-09 14:56:55 -0600

    Attempt 3 for server.js modification... I think I got it

--- server.js
+++ server.js
@@ -105,7 +105,16 @@ var SampleApp = function() {
         self.routes['/'] = function(req, res) {
             res.setHeader('Content-Type', 'text/html');
             res.send(self.cache_get('index.html') );
-            res.send(self.cache_get('resume.html') );
+        };
+
+        self.routes['Resume'] = function(req, res) {
+          res.setHeader('Content-Type', 'text/html');
+          res.send(self.cache_get('resume.html') );
+        };
+
+        self.routes['Resume/photos'] = function(req, res) {
+          res.setHeader('Content-Type', 'text/html');
+          res.send(self.cache_get('photos.html') );
         };
     };
 
