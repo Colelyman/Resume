2f5a0dc556e0602be72edfbaed2ad3749ad84954
Cole Lyman <cole@colelyman.com>
2015-09-09 14:53:31 -0600

    Attempt 2 for server.js modification

--- server.js
+++ server.js
@@ -105,6 +105,7 @@ var SampleApp = function() {
         self.routes['/'] = function(req, res) {
             res.setHeader('Content-Type', 'text/html');
             res.send(self.cache_get('index.html') );
+            res.send(self.cache_get('resume.html') );
         };
     };
 
